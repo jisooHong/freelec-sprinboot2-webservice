945579a42aa33ac06f890180a843af9b6a963684
jisooHong <ghdwltn0204@naver.com>
2022-02-19 17:48:09 +0900

    [add] 구글 로그인 기능 구현

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()//url별 권환 관리를 설정하는 옵션의 시작점
+                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()//전체 열람 권한 부여
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())//USER 권한을 가진 사람만 열람 가능
+                    .anyRequest().authenticated()// 그 이외의 url들은 모두 인증된 사용자들에게만 허용하게 한다.(로그인한 사용자들)
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()//로그인 성공 이후 사용자 정보를 가져올 때 설정들
+                            .userService(customOAuth2UserService);//소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체 등록
+
+    }
+}
