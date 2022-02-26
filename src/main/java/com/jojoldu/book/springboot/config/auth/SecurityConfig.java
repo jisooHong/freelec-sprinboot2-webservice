5462a120db52052cbc6bf66b54dda21db2e5150b
jisooHong <ghdwltn0204@naver.com>
2022-02-27 01:29:47 +0900

    [add] ProfileController , test 추가

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()//url별 권환 관리를 설정하는 옵션의 시작점
-                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()//전체 열람 권한 부여
+                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**","/profile").permitAll()//전체 열람 권한 부여
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())//USER 권한을 가진 사람만 열람 가능
                     .anyRequest().authenticated()// 그 이외의 url들은 모두 인증된 사용자들에게만 허용하게 한다.(로그인한 사용자들)
                 .and()
